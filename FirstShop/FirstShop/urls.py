1b3462575792a69f46cb0ff27254dca70c563301
JhinDanzo <gentj03@icloud.com>
2019-11-07 13:44:37 +0200

    urls

--- FirstShop/FirstShop/urls.py
+++ FirstShop/FirstShop/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     url(r'^register/$', views.RegisterFormView.as_view()),
     url(r'^login/$', views.LoginFormView.as_view()),
     url(r'^logout/$', views.LogoutView.as_view()),
-
 ]
